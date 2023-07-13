943f34dca51ff14c056263a940440d61356aedb1
Trinity Scales <trinityscales@yahoo.com>
2023-07-13 18:21:37 -0500

    US-05 Complete
    Timer added

--- src/index.js
+++ src/index.js
@@ -181,9 +181,10 @@ function clearScore() {
 *
 */
 function updateTimer() {
-  // TODO: Write your code here.
-  // hint: this code is provided to you in the instructions.
-  
+  if (time > 0) {
+    time -= 1;
+    timerDisplay.textContent = time;
+  }
   return time;
 }
 
@@ -194,8 +195,7 @@ function updateTimer() {
 *
 */
 function startTimer() {
-  // TODO: Write your code here
-  // timer = setInterval(updateTimer, 1000);
+  timer = setInterval(updateTimer, 1000);
   return timer;
 }
 
